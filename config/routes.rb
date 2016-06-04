7a9c4de9b3670f6a7c0d29dc5320ad3c495613af
Hakim Ariffin <hakimishere@gmail.com>
2016-06-04 22:27:04 +0800

    Changes root of application for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  root 'welcome#index'
 
   get 'welcome/index'
 
   resources :docs
+
+  authenticated :user do 
+    root "docs#index", as: "authenticated_root"
+  end
+
+  root 'welcome#index'
 
   # Syntax for only - ", only: [:show, :edit]"
 
