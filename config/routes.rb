8b6dade12295375880c2acd8cfce7600d0946d4a
Hakim Ariffin <hakimishere@gmail.com>
2016-06-04 15:41:57 +0800

    Adds Doc controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
+  resources :docs
+
+  # Syntax for only - ", only: [:show, :edit]"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
